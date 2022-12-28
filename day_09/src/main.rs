9e37d77c1d810c899fd4580c7cf2216e08f95117
Duarte OC <duarteocarmo@gmail.com>
2022-12-28 14:45:52 +0100

    day 09

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,156 +1,51 @@
-use hashbrown::HashSet;
-use nalgebra::distance;
-use nalgebra::point;
-use nalgebra::geometry::Point2;
-use nalgebra::vector;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
-use std::path::Path;
+use std::collections::HashSet;
 
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("no such file");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
+use std::{env, fs};
+
+fn read_file_input(file: String) -> String {
+    let cwd = env::current_dir().unwrap();
+    let filepath = cwd.join("./").join(file);
+
+    fs::read_to_string(filepath).unwrap()
 }
-
-// ---
-
-fn part_1() {
-    let lines = lines_from_file("./example.txt");
-    let mut head_position = point!(0.0, 0.0);
-    let mut tail_position = point!(0.0, 0.0);
-
-    for (step, line) in lines.iter().enumerate() {
-        let (direction, count) = line.split_once(" ").unwrap();
-        let count = count.parse::<i32>().unwrap();
-
-        let out = match direction {
-            "R" => (1.0, 0.0),
-            "L" => (-1.0, 0.0),
-            "U" => (0.0, 1.0),
-            "D" => (0.0, -1.0),
-            _ => panic!("SHIT HIT THE FAN"),
-        };
-
-        // let direction = vec2(out.0, out.1);
-        //
-        let direction = vector![out.0, out.1];
-
-        println!("========START STEP {:?}=======", step + 1);
-        // println!("HEAD AT: {:?}", head_position);
-        // println!("TAIL AT: {:?}", tail_position);
-
-
-        let mut switch_next_step = false;
-        let mut fixer = point!(0.0, 0.0);
-
-        for mini_step in 0..count {
-            head_position += direction;
-            println!("HEAD TO: {:?}", head_position);
-            if (mini_step == 0) & (step == 0) {
-                println!("~~");
-                continue;
-            }
-
-            let diagonal =
-                (head_position.x != tail_position.x) & (head_position.y != tail_position.y);
-
-            if switch_next_step == true {
-                tail_position = fixer;
-                switch_next_step = false;
-            } else if diagonal == true {
-                println!("DIAGONAL");
-                switch_next_step = true;
-                fixer = head_position;
-                // fixer = head_position;
-            } else {
-                tail_position += direction;
-            }
-
-            println!("TAIL TO: {:?}", tail_position);
-            println!("~~")
-            // println!("STEP {:?}, HEAD {:?}", mini_step, head_position);
-        }
-
-        println!("===== END STEP =====")
-    }
-}
-
-fn part_2() {}
-
-
 
 fn main() {
-    println!("{:?}", part_1());
-    // println!("{:?}", part_2());
+    let input = read_file_input("example.txt".to_string());
+
+    println!("{}", calculate(&input, 1));
+    println!("{}", calculate(&input, 9));
 }
 
+fn calculate(input: &String, length: usize) -> usize {
+    let mut rope = vec![(0i32, 0i32); length + 1];
+    let mut visited = HashSet::new();
+    visited.insert((0, 0));
 
-use std::vec;
+    for line in input.lines() {
+        let instruction = line.split_once(' ').unwrap();
 
-use hashbrown::HashSet;
+        let (dx, dy) = match instruction.0 {
+            "U" => (0, 1),
+            "D" => (0, -1),
+            "R" => (1, 0),
+            "L" => (-1, 0),
+            _ => unreachable!(),
+        };
 
-use crate::{problem, Solution};
+        let steps = instruction.1.parse::<usize>().unwrap();
 
-type Point = aoc_lib::Point<i32>;
-
-pub struct Day09;
-
-impl Solution for Day09 {
-    fn name(&self) -> &'static str {
-        "Rope Bridge"
-    }
-
-    fn part_a(&self) -> String {
-        let raw = problem::load(2022, 9);
-        process(&raw, 1).to_string()
-    }
-
-    fn part_b(&self) -> String {
-        let raw = problem::load(2022, 9);
-        process(&raw, 9).to_string()
-    }
-}
-
-fn process(raw: &str, count: usize) -> usize {
-    let orders = raw.lines().map(from_line).collect::<Vec<_>>();
-    let mut tail_pios = HashSet::new();
-    let mut knots = vec![Point::new(0, 0); count + 1];
-
-    tail_pios.insert(*knots.last().unwrap());
-    for (dir, count) in orders {
-        for _ in 0..count {
-            knots[0] += dir;
-
-            for i in 1..knots.len() {
-                let diff = knots[i - 1] - knots[i];
-                if diff.abs().max_value() <= 1 {
-                    continue;
+        for _ in 0..steps {
+            rope[0] = (rope[0].0 + dx, rope[0].1 + dy);
+            for i in 1..rope.len() {
+                let (dx, dy) = (rope[i - 1].0 - rope[i].0, rope[i - 1].1 - rope[i].1);
+                if dx.abs() > 1 || dy.abs() > 1 {
+                    rope[i].0 += dx.signum();
+                    rope[i].1 += dy.signum();
                 }
-
-                knots[i] += diff.normalize();
             }
-            tail_pios.insert(*knots.last().unwrap());
+            visited.insert(rope[length]);
         }
     }
 
-    tail_pios.len()
-}
-
-// Direction, count
-fn from_line(imp: &str) -> (Point, u32) {
-    let (direction, count) = imp.split_once(' ').unwrap();
-    let count = count.parse::<i32>().unwrap();
-
-    let out = match direction {
-        "R" => Point::new(1, 0),
-        "L" => Point::new(-1, 0),
-        "U" => Point::new(0, -1),
-        "D" => Point::new(0, 1),
-        _ => panic!("Invalid direction"),
-    };
-
-    (out, count as u32)
+    return visited.len();
 }
