1ec07b5b279d29e309a600646d7a2d1d4bb96530
Duarte OC <duarteocarmo@gmail.com>
2022-12-29 20:58:26 +0100

    day 10

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -58,7 +58,71 @@ fn part_1() {
     println!("RESULT {:?}", signal_strength)
 }
 
+fn draw_sprite(position: i32, cycle_count: i32) {
+    let positions = vec![position - 1, position, position + 1];
+    let mut zero_vec = vec![0; 40];
+
+    for p in positions {
+        if p >= 0 {
+            zero_vec[p as usize] = 1;
+        }
+    }
+
+    println!(
+        "SPRITE \t(POS: {:?}; CYC: {:?})\t\t: {:?}",
+        position, cycle_count, zero_vec
+    );
+}
+
+fn draw_pixel(cycle_count: i32, sprite_center: i32) {
+    let upp_bound: Vec<u32> = (1..240).step_by(40).collect();
+    let down_bound: Vec<u32> = (40..=240).step_by(40).collect();
+    for du in down_bound.iter().zip(upp_bound.iter()) {
+        let (d, u) = du;
+        if (cycle_count >= *d as i32) & (cycle_count <= *u as i32) {
+        } else {
+        }
+    }
+
+    println!("{:?}", upp_bound);
+    println!("{:?}", down_bound);
+}
+
+fn part_2() {
+    let lines = lines_from_file("./example.txt");
+
+    let mut register_value = 1;
+    let mut cycle_count = 0;
+    let mut signal_strength = 0;
+    let mut sprite_center = 1;
+
+    for line in lines {
+        if line == "noop" {
+            // inspect_signal(cycle_count, register_value);
+            draw_sprite(sprite_center, cycle_count);
+            draw_pixel(cycle_count, sprite_center);
+            cycle_count += 1;
+            continue;
+        }
+
+        if line.contains("addx") {
+            let (_, count) = line.split_once(" ").unwrap();
+            let count = count.parse::<i32>().unwrap();
+
+            cycle_count += 1;
+            draw_sprite(sprite_center, cycle_count);
+            cycle_count += 1;
+            sprite_center += count;
+            draw_sprite(sprite_center, cycle_count);
+            continue;
+        }
+    }
+    // inspect_signal(cycle_count, register_value);
+    //
+}
 fn main() {
-    part_1();
+    // part_1();
     // inspect_signal(140, 3123);
+    // part_2();
+    draw_pixel(32, 32);
 }
